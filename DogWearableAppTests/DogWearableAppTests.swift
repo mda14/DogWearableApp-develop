cf10f9c4111a1e7d488001a35a39170f001ee3a0
mda14 <Chispi@air-de-rocio.home>
2017-12-15 12:01:17 +0000

    Initial Commit
    Two screens and two controllers. Segues between screens.

---
+++ DogWearableAppTests/DogWearableAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  DogWearableAppTests.swift
+//  DogWearableAppTests
+//
+//  Created by Chispi on 09/11/2017.
+//  Copyright © 2017 WearablesGuder. All rights reserved.
+//
+
+import XCTest
+@testable import DogWearableApp
+
+class DogWearableAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ StartVC.swift
@@ -0,0 +1,30 @@
+//
+//  StartVC.swift
+//  DogWearableApp
+//
+//  Created by Chispi on 13/12/2017.
+//  Copyright © 2017 WearablesGuder. All rights reserved.
+//
+
+import UIKit
+
+class StartVC: UIViewController {
+    @IBOutlet weak internal var StartBtn: UIButton!
+    @IBOutlet weak var BackBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func BackBtn(_ sender: Any) {
+    }
+    @IBAction func createUdpConnection() {
+        // create udp connection to board
+    }
+}
