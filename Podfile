e076eb68fd12fccbc273074dc0cfa6c52ef6685a
mda14 <Chispi@air-de-rocio.home>
2018-01-30 19:31:22 +0000

    UDP connection
    UDP Connection using SwiftSocket

--- Podfile
+++ Podfile
@@ -3,6 +3,8 @@ platform :ios, '11.0'
 target 'DogWearableApp' do
   use_frameworks!
     pod 'SwiftSocket'
+    pod 'CocoaAsyncSocket'
+    
   # Pods for DogWearableApp
 
   target 'DogWearableAppTests' do
--- StartVC.swift
+++ StartVC.swift
@@ -9,9 +9,11 @@
 import UIKit
 import SwiftSocket
 
-class StartVC: UIViewController {
-    @IBOutlet weak internal var StartBtn: UIButton!
+class StartVC: UIViewController  {
+    @IBOutlet weak var StartBtn: UIButton!
     @IBOutlet weak var BackBtn: UIButton!
+    @IBOutlet weak var ConnectBtn: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +26,31 @@ class StartVC: UIViewController {
     }
     
     @IBAction func BackBtn(_ sender: Any) {
+        print("Back Button")
     }
-    @IBAction func createUdpConnection() {
-        // create udp connection to board
+    
+    @IBAction func connectToBoard(_ sender: Any) {
+        print("ConnectToBoard")
+        //check if user is connected to guderesearch WiFi
+        //if not, promt user to connect
+
+            }
+    
+    @IBAction func startUdpConnection(_ sender: Any) {
+
+        // Create UDP socket that connects to address and port of ESP32 board
+        let client = UDPClient(address: "192.168.4.1", port: 3333)
+        print("Client address:  \(client.address)")
+        client.enableBroadcast()
+
+        // Send 'start' string for board to start sending data
+        let data = "start" // ... Bytes you want to send
+        let result = client.send(string: data)
+        print("Result: \(result.isSuccess)")
+        let incoming = client.recv(1024*10)
+        // to view message printed need to decode
+        let incoming_print = String(bytes: incoming.0!, encoding: String.Encoding.utf8)
+        print("Data received: \(String(describing: incoming_print))")
     }
+    
 }
